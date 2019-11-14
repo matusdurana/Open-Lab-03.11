94e14d915378172270595658eb878fdf4868fa54
MatusDurana <matusdurana2@gmail.com>
2019-11-14 20:08:42 +0100

    hotove

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            string ispalindrome = str.Substring(0, str.Length);
+            char[] arr = str.ToCharArray();
+            
+            Array.Reverse(arr);
+            
+            string temp = new string(arr);
+            string first = temp.Substring(0, temp.Length);
+
+            return str.Equals(first);
         } 
     }
 }
